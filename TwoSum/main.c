cc8892ba4c0475a97a53378331209b1115671512
ROBLOXHACCKER <emailcreatasulmomento@gmail.com>
2024-12-23 13:40:49 +0100

    TwoSum - subject

---
+++ TwoSum/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    while(*nums)
+    {
+
+    }
+}
+
+
+int main()
+{
+    int nums[4] = {2,7,11,15};
+    int numSize = sizeof(nums) / sizeof(nums[0]);
+    int *nump = nums;
+    int target = 9;
+
+    twoSum(nums, numSize, target, 2);
+
+
+
+
+
+
+}

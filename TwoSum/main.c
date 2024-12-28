6cc4bc35eb5f14c08ebf4023c5e44dfba0be9dbd
ROBLOXHACCKER <emailcreatasulmomento@gmail.com>
2024-12-28 02:10:10 +0100

    fine TwoSum vera

--- TwoSum/main.c
+++ TwoSum/main.c
@@ -1,37 +1,21 @@
-#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
 
-void twoSum(int* nums, int numsSize, int target) {
-    int count = 0;
-    while (count < numsSize) {
-        int i = count + 1;
-        while (i < numsSize) {
-            if (nums[count] + nums[i] == target) {
-                // Stampa i due numeri trovati
-                printf("I due numeri trovati sono: %d e %d\n", nums[count], nums[i]);
-                return; 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int count = 0;
+        while (count < nums.size()) {
+            int i = count + 1;
+            while (i < nums.size()) {
+                if (nums[count] + nums[i] == target) {
+                    return {count, i}; 
+                }
+                i++;
             }
-            i++;
+            count++;
         }
-        count++;
+        return {};
     }
-    printf("Nessuna coppia trovata.\n"); 
-}
-
-
-
-
-int main()
-{
-    int nums[5] = {1,2,2,4,15};
-    int numSize = sizeof(nums) / sizeof(nums[0]);
-    int *nump = nums;
-    int target = 4;
-
-    twoSum(nums, numSize, target, 2);
-
-
-
-
-
-
-}
+};
